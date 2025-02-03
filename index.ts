0dd15836008d976bf88445e8e6f17f23d727cce2
gideon <kipkerring.gedion@gmail.com>
2025-02-03 20:04:05 +0300

    test

--- index.ts
+++ index.ts
@@ -2,3 +2,16 @@ let tshard: boolean = false;
 console.log(tshard);
 let tsstring: string = "Gideon";
 console.log(tsstring);
+
+const multiply = (x: number, y: number) => x * y;
+const result = multiply(8, 7);
+
+console.log(result);
+
+// factory pattern
+const Person: { firstName: string; lastName: string; age: number } = {
+  firstName: "Gideon",
+  lastName: "Sitienei",
+  age: 30,
+};
+console.log(`Name: ${Person.firstName} ${Person.lastName}, Age: ${Person.age}`);
