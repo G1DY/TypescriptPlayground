7612319d73e7a4b85dde0d4ef49d8cc4359f6472
gideon <kipkerring.gedion@gmail.com>
2025-02-04 21:40:08 +0300

    understanding generics in TS

--- generics.ts
+++ generics.ts
@@ -1,7 +1,7 @@
 function uniqueDataTypes<T>(item: T, defaultValue: T): [T, T] {
   return [item, defaultValue];
 }
-const num = uniqueDataTypes(1, 3);
-const str = uniqueDataTypes("hello", "world");
-const bool = uniqueDataTypes(true, false);
+const num = uniqueDataTypes<number>(1, 3);
+const str = uniqueDataTypes<string>("hello", "world");
+const bool = uniqueDataTypes<boolean>(true, false);
 console.log(num, str, bool);
