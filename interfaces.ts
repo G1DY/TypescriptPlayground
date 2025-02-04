e1bdb6cf427adce3b4a3b8be538f01b7a90af84a
gideon <kipkerring.gedion@gmail.com>
2025-02-04 19:52:11 +0300

    understanding interfaces in typescript

--- interfaces.ts
+++ interfaces.ts
@@ -16,3 +16,30 @@ class car implements vehicle {
 const myCar = new car();
 myCar.start();
 myCar.stop();
+
+// interface example 2
+interface Movie {
+  readonly name: string;
+  rating: number;
+  genre?: string;
+}
+const movie1: Movie = {
+  name: "star wars",
+  rating: 10,
+  genre: "comedy",
+};
+console.log(movie1);
+console.log(movie1.name);
+
+// interface as a function
+interface mathOperations {
+  (x: number, y: number): number;
+}
+const add: mathOperations = (a, b) => a + b;
+const subtract: mathOperations = (a, b) => a - b;
+const multiplication: mathOperations = (a, b) => a * b;
+let a = 14,
+  b = 7;
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiplication(a, b));
