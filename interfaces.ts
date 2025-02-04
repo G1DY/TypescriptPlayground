bb08c6c9ea4d688542708cae268290d49db3cef9
gideon <kipkerring.gedion@gmail.com>
2025-02-04 19:00:56 +0300

    implementing interfaces in TS

--- interfaces.ts
+++ interfaces.ts
@@ -1,7 +1,9 @@
+// interface for a class with methods
 interface vehicle {
   start(): void;
   stop(): void;
 }
+// the class implementing the methods
 class car implements vehicle {
   start() {
     console.log("Car is starting");
@@ -10,6 +12,7 @@ class car implements vehicle {
     console.log("car is stoping");
   }
 }
+// creating an object instance
 const myCar = new car();
 myCar.start();
 myCar.stop();
