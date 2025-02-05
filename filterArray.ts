1d3bb1b9e77cd73df50402deefcff9db55858d21
gideon <kipkerring.gedion@gmail.com>
2025-02-05 12:11:17 +0300

    filtering an array in TS

--- filterArray.ts
+++ filterArray.ts
@@ -4,3 +4,23 @@ function filterArray<T>(array: T[], condition: (item: T) => boolean): T[] {
 const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evennumbers = filterArray<number>(numberArray, (num) => num % 2 === 0);
 console.log(evennumbers);
+
+const strArray = ["apple", "banana", "cherry", "date"];
+const shortStr = filterArray<string>(strArray, (word) => word.length < 6);
+console.log(shortStr);
+
+// using with an interface
+interface Fruits {
+  name: string;
+  color: string;
+}
+const fruitArray: Fruits[] = [
+  { name: "apple", color: "red" },
+  { name: "banana", color: "yellow" },
+  { name: "cherry", color: "red" },
+];
+const redFruits = filterArray<Fruits>(
+  fruitArray,
+  (fruit) => fruit.color === "red"
+);
+console.log(redFruits);
