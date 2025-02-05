fce538f90c466a3044dd12f72d14ece120a5f819
gideon <kipkerring.gedion@gmail.com>
2025-02-05 12:40:14 +0300

    using generics in classes

--- genericClass.ts
+++ genericClass.ts
@@ -16,3 +16,9 @@ class Box<T> {
 }
 const stringBox = new Box<string>("hello, typescript");
 console.log(stringBox.getContent());
+stringBox.setContent("new content added");
+console.log(stringBox.getContent());
+const numberBox = new Box<number>(20);
+console.log(numberBox.getContent());
+numberBox.setContent(100);
+console.log(numberBox.getContent());
