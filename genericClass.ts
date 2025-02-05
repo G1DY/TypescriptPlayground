0fd3d580e00a97b76d22fe4c788c3957a7ddba70
gideon <kipkerring.gedion@gmail.com>
2025-02-05 12:28:39 +0300

    using generics in classes

---
+++ genericClass.ts
@@ -0,0 +1,18 @@
+// using generics in classes
+class Box<T> {
+  private content: T;
+  /**
+   *
+   */
+  constructor(initialContent: T) {
+    this.content = initialContent;
+  }
+  getContent(): T {
+    return this.content;
+  }
+  setContent(newContent: T): void {
+    this.content = newContent;
+  }
+}
+const stringBox = new Box<string>("hello, typescript");
+console.log(stringBox);
